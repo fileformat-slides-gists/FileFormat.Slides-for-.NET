08fd35d09d0d4487f4fe378e283e8abb6251feb0
Muhammad Umar <muhammad.umar@groupdocs.com>
2024-01-24 12:35:39 +0500

    Released 24.1.0

--- FileFormat.Slides.Examples.Usage/Program.cs
+++ FileFormat.Slides.Examples.Usage/Program.cs
@@ -7,21 +7,35 @@ namespace FileFormat.Slides.Examples.Usage
     {
         static void Main (string[ ] args)
         {
-            // SlideExamples slideExamples = new SlideExamples();
-            // slideExamples.CreateNewSlideInNewPresentation();
+            //SlideExamples slideExamples = new SlideExamples();
+            
+            //slideExamples.CreateNewSlideInNewPresentation();
 
-            // slideExamples.CreateNewSlideInExistingPresentation(filename:"test.pptx");
+            //slideExamples.CreateNewSlideInExistingPresentation(filename:"test.pptx");
 
-            // slideExamples.RemoveSlideInAnExistingPresentation(filename: "test.pptx");
+            //slideExamples.RemoveSlideInAnExistingPresentation(filename: "test.pptx");
+
+            //slideExamples.AddBackgroundColorToAnExistingSlide(filename: "sample.pptx");
 
             //TextExamples textExamples = new TextExamples();
+            
             //textExamples.CreateNewTextShapeInNewSlide();
+            
             //textExamples.AddNewTextShapeExistingSlide(filename: "sample.pptx");
 
-            ImageExamples imageExamples = new ImageExamples();
+            //ImageExamples imageExamples = new ImageExamples();
+            
             //imageExamples.AddImageInASlide(imagename:"sample.jpg");
-            imageExamples.UpdateImageInExistingSlide(filename: "sample.pptx", xAxis: 300.0, yAxis: 200.0);
+            
+            //imageExamples.UpdateImageInExistingSlide(filename: "sample.pptx", xAxis: 300.0, yAxis: 200.0);
 
+            //StyledListExamples styledListExamples = new StyledListExamples();
+            
+            //styledListExamples.CreateBulletedListInASlide();
+            
+            //styledListExamples.AddListItemsInAnExistingList(filename: "test.pptx");
+            
+            //styledListExamples.RemoveListItemsInAnExistingList(filename: "test.pptx");
         }
     }
 }
--- FileFormat.Slides.Examples/StyledListExamples.cs
+++ FileFormat.Slides.Examples/StyledListExamples.cs
@@ -1,4 +1,5 @@
 ﻿using FileFormat.Slides.Common;
+using FileFormat.Slides.Common.Enumerations;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,7 +10,7 @@ namespace FileFormat.Slides.Examples
     /// Provides C# code examples for creating, reading, and modifying slides in a Presentation
     /// using the <a href="https://www.nuget.org/packages/FileFormat.Slides">FileFormat.Slides</a> library.
     /// </summary>
-    public class SlideExamples
+    public class StyledListExamples
     {
         private const string newDocsDirectory = "../../../Presentations/New";
         private const string existingDocsDirectory = "../../../Presentations/Existing";
@@ -20,7 +21,7 @@ namespace FileFormat.Slides.Examples
         /// If the directory doesn't exist, it is created. If it already exists,
         /// existing files are deleted, and the directory is cleaned up.
         /// </summary>
-        public SlideExamples ()
+        public StyledListExamples ()
         {
             if (!System.IO.Directory.Exists(newDocsDirectory))
             {
@@ -42,60 +43,35 @@ namespace FileFormat.Slides.Examples
             }
         }
         /// <summary>
-        /// This method create slides in a new presentation
+        /// This method creates bulleted list in a slide
         /// </summary>
         /// <param name="documentDirectory">Path of the presentation folder</param>
         /// <param name="filename">Presentation name</param>
-        public void CreateNewSlideInNewPresentation (string documentDirectory = newDocsDirectory, string filename = "test.pptx")
+        public void CreateBulletedListInASlide (string documentDirectory = newDocsDirectory, string filename = "test.pptx")
         {
             try
             {
                 // Create instance of presentation
                 Presentation presentation = Presentation.Create($"{documentDirectory}/{filename}");
-                //Create instances of text shapes and set their texts.
+                //Create instances of text shapes and set their properties.
                 TextShape shape = new TextShape();
-                shape.Text = "Title: Here is my first title From FF";
+                shape.Y = 20.0;
+                shape.FontSize = 80;
+                shape.FontFamily = "Baguet Script";
+                shape.Text = "Bulleted List Example";
                 TextShape shape2 = new TextShape();
-                shape2.Text = "Body : Here is my first title From FF";    
-                // Set yAxis of 2nd text shape
-                shape2.Y = 25.9;
+                shape2.FontSize = 100;
+                shape2.Y = 180.0;
+                // Create instance of Bulleted List
+                StyledList list = new StyledList(ListType.Bulleted);
+                // Add items to list
+                list.AddListItem("USA");
+                list.AddListItem("Canada");
+                list.AddListItem("Brazil");
+                list.AddListItem("Mexico");
+                // Assign list to text shape
+                shape2.TextList = list;
                 // Create slide
-                Slide slide = new Slide();
-                // Set background color of slide because default background color is black.
-                slide.BackgroundColor = Colors.Silver;
-                // Add text shapes.
-                slide.AddTextShapes(shape);
-                slide.AddTextShapes(shape2);               
-                // Adding slides
-                presentation.AppendSlide(slide); 
-                // Save presentation
-                presentation.Save();
-
-            }
-            catch (System.Exception ex)
-            {
-                throw new FileFormat.Slides.Common.FileFormatException("An error occurred.", ex);
-            }
-        }
-        /// <summary>
-        /// This method creates new slide in an existing presentation
-        /// </summary>
-        /// <param name="documentDirectory">Path of the presentation folder</param>
-        /// <param name="filename">Presentation name</param>
-        public void CreateNewSlideInExistingPresentation (string documentDirectory = existingDocsDirectory, string filename = "test.pptx")
-        {
-            try
-            {
-
-                // Create instance of presentation
-                Presentation presentation = Presentation.Open($"{documentDirectory}/{filename}");
-                //Create instances of text shapes and set their texts.
-                TextShape shape = new TextShape();
-                shape.Text = "Title: Here is my first title From FF";
-                TextShape shape2 = new TextShape();
-                shape2.Text = "Body : Here is my first title From FF";
-                shape2.Y = 25.9;
-                // Create new slide
                 Slide slide = new Slide();
                 // Set background color of slide because default background color is black.
                 slide.BackgroundColor = Colors.Silver;
@@ -114,18 +90,45 @@ namespace FileFormat.Slides.Examples
             }
         }
         /// <summary>
-        /// This method removes slide in an existing presentation
+        /// This method creates numbered list in a slide
         /// </summary>
         /// <param name="documentDirectory">Path of the presentation folder</param>
         /// <param name="filename">Presentation name</param>
-        public void RemoveSlideInAnExistingPresentation (string documentDirectory = existingDocsDirectory, string filename = "test.pptx")
+        public void CreateNumberedListInASlide (string documentDirectory = existingDocsDirectory, string filename = "test.pptx")
         {
             try
             {
+
                 // Create instance of presentation
-                Presentation presentation = Presentation.Open($"{documentDirectory}/{filename}");
-                // Remove slide at first index
-                presentation.RemoveSlide(0);
+                Presentation presentation = Presentation.Create($"{documentDirectory}/{filename}");
+                //Create instances of text shapes and set their properties.
+                TextShape shape = new TextShape();
+                shape.Y = 20.0;
+                shape.FontSize = 80;
+                shape.FontFamily = "Amasis MT Pro Black";
+                shape.Text = "Numbered List Example";
+                TextShape shape2 = new TextShape();
+                shape2.FontSize = 100;
+                shape2.Y = 180.0;
+                // Create instance of Bulleted List
+                StyledList list = new StyledList(ListType.Bulleted);
+                // Add items to list
+                list.AddListItem("Tennis");
+                list.AddListItem("Cricket");
+                list.AddListItem("Hockey");
+                list.AddListItem("Football");
+                list.AddListItem("Snooker");
+                // Assign list to text shape
+                shape2.TextList = list;
+                // Create slide
+                Slide slide = new Slide();
+                // Set background color of slide because default background color is black.
+                slide.BackgroundColor = Colors.Silver;
+                // Add text shapes.
+                slide.AddTextShapes(shape);
+                slide.AddTextShapes(shape2);
+                // Adding slides
+                presentation.AppendSlide(slide);
                 // Save presentation
                 presentation.Save();
 
@@ -135,19 +138,61 @@ namespace FileFormat.Slides.Examples
                 throw new FileFormat.Slides.Common.FileFormatException("An error occurred.", ex);
             }
         }
-        public void AddBackgroundColorToAnExistingSlide (string documentDirectory = existingDocsDirectory, string filename = "test.pptx")
+        /// <summary>
+        /// This method adds the list items in an existing numbered or bulleted list.
+        /// </summary>
+        /// <param name="documentDirectory">Path of the presentation folder</param>
+        /// <param name="filename">Presentation name</param>
+        public void AddListItemsInAnExistingList(string documentDirectory = existingDocsDirectory, string filename = "test.pptx")
         {
-            
+            try
+            {                
+                // Create instance of presentation
+                Presentation presentation = Presentation.Open($"{documentDirectory}/{filename}");
+                // Get desired slide
+                Slide slide = presentation.GetSlides()[3];
+                // Create instance of Bulleted List
+                TextShape shape = slide.TextShapes[0];
+                StyledList list = shape.TextList;
+                // Add items to list
+                list.AddListItem("Tennis");
+                list.AddListItem("Cricket");
+                list.AddListItem("Hockey");
+                list.AddListItem("Football");
+                list.AddListItem("Snooker");
+                // Update the list
+                list.Update();
+                // Save presentation
+                presentation.Save();
+
+            }
+            catch (System.Exception ex)
+            {
+                throw new FileFormat.Slides.Common.FileFormatException("An error occurred.", ex);
+            }
+        }
+        /// <summary>
+        /// This method removes the list items in an existing numbered or bulleted list.
+        /// </summary>
+        /// <param name="documentDirectory">Path of the presentation folder</param>
+        /// <param name="filename">Presentation name</param>
+        public void RemoveListItemsInAnExistingList (string documentDirectory = existingDocsDirectory, string filename = "test.pptx")
+        {
             try
             {
                 // Create instance of presentation
                 Presentation presentation = Presentation.Open($"{documentDirectory}/{filename}");
                 // Get desired slide
-                Slide slide = presentation.GetSlides()[0];
-                // Set background color
-                slide.BackgroundColor = Colors.Fuchsia;
-                // Update slide
-                slide.Update();
+                Slide slide = presentation.GetSlides()[3];
+                // Create instance of Bulleted List
+                TextShape shape = slide.TextShapes[0];
+                StyledList list = shape.TextList;
+                // Remove a range of list items
+                list.ListItems.RemoveRange(0,4);
+                // Or you can remove all items like 
+                list.ListItems.RemoveAt(1);
+                // Update the list
+                list.Update();
                 // Save presentation
                 presentation.Save();
 
@@ -156,8 +201,8 @@ namespace FileFormat.Slides.Examples
             {
                 throw new FileFormat.Slides.Common.FileFormatException("An error occurred.", ex);
             }
-
         }
+
     }
 
 }
